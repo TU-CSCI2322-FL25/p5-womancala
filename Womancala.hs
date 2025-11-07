a37b5136b6e22a62078fd706debf7f959857df8e
pgrantz-trinity <pgrantz@trinity.edu>
2025-11-07 15:24:23 -0600

    Pretty Print is Pretty

--- Womancala.hs
+++ Womancala.hs
@@ -27,41 +27,42 @@ type Game = (Turn, Board)
 --This is primarily for debugging purposes.
 --Template:
 -- Current turn: P_
--- -----------------------------
--- | |   |   |   |   |   |   | |
--- | |-----------------------| |
--- | |   |   |   |   |   |   | |
--- -----------------------------
+-- -----------------------------------------
+-- |    |    |    |    |    |    |    |    |
+-- |    |-----------------------------|    |
+-- |    |    |    |    |    |    |    |    |
+-- -----------------------------------------
 
 testBoard :: Board
 testBoard = [(0,0),(1,4),(2,4),(3,4),(4,4),(5,4),(6,4),(7,0),(8,4),(9,4),(10,4),(11,4),(12,4),(13,4)]
 testBoardOne :: Board
-testBoardOne = [(0,0),(1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(7,7),(8,8),(9,9),(10,0),(11,1),(12,2),(13,3)]
-
-
-edgeOfBoard = "-----------------------------"
+testBoardOne = [(0,20),(1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(7,20),(8,8),(9,9),(10,10),(11,11),(12,12),(13,13)]
 
 prettyPrint :: Game -> String
 prettyPrint (turn,board) = "Current turn: "++(printPlayer currentTurn)++"\n"++
-                            edgeOfBoard++"\n"++
-                            "|"++(prettyPrintSide board (reverse sideTwo))++"\n"++
+                            "-----------------------------------------"++"\n"++
+                            "|   "++(prettyPrintSide board (reverse sideTwo))++"\n"++
                             (prettyPrintMiddle board)++"\n"++
-                            "|"++(prettyPrintSide board sideOne)++"\n"++
-                            edgeOfBoard++ "\n"
-
-prettyPrintSide :: Board -> [Index]-> String --Will print a side minus the first "|"
-prettyPrintSide board [] = " | |"
-prettyPrintSide board (index:indexes) = " | "++(show (myLookup index board)) ++ prettyPrintSide board indexes
-
-prettyPrintMiddle :: Board -> String
-prettyPrintMiddle board = "|" ++ show (myLookup storeTwo board) ++ "|-----------------------|" ++ show (myLookup storeOne board) ++ "|"
-
-myLookup :: Eq a => a -> [(a, b)] -> b
-myLookup key list = result
-    where (Just result) = lookup key list
-
-printPlayer :: Player -> String
-printPlayer P1 = "P1" 
-printPlayer P2 = "P2"
+                            "|   "++(prettyPrintSide board sideOne)++"\n"++
+                            "-----------------------------------------"++ "\n"
+  where --
+        prettyPrintSide :: Board -> [Index]-> String --Will print a side minus the first "|"
+        prettyPrintSide board [] = " |    |"
+        prettyPrintSide board (index:indexes) = " | "++(spacedLookup index board) ++ prettyPrintSide board indexes
+        --
+        prettyPrintMiddle :: Board -> String
+        prettyPrintMiddle board = "| " ++
+                                  (spacedLookup storeTwo board) ++
+                                  " |-----------------------------| " ++
+                                  (spacedLookup storeOne board) ++
+                                  " |"
+        --
+        spacedLookup :: Int -> Board -> String
+        spacedLookup key list = if result>9 then show result else " "++(show result)
+            where (Just result) = lookup key list
+        --
+        printPlayer :: Player -> String
+        printPlayer P1 = "P1" 
+        printPlayer P2 = "P2"
 
 ----------------------------------------
