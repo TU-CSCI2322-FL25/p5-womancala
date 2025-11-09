010d011c749cbea4903b534a6c70e6d325561000
scox3 <scox3@trinity.edu>
2025-11-08 22:15:33 -0600

    Finished the completeMove function for now, no error handling or game over detection

--- Womancala.hs
+++ Womancala.hs
@@ -9,23 +9,67 @@ storeTwo = 0 --Store index for P2
 sideOne = [1..6] --Indexes for P1 pits
 sideTwo = [8..13] -- Indexes for P2 pits
 type Board = [Pit] -- Will only ever be 14 long
-data Player = P1 | P2
+data Player = P1 | P2 deriving Eq
 type Turn = Player
-currentTurn :: Turn
-currentTurn = P1
 type Winner = Maybe Player
 type Move = Index
 type Game = (Turn, Board)
 
+initialState :: Game
+initialState = (P1,[(0,0),(1,4),(2,4),(3,4),(4,4),(5,4),(6,4),(7,0),(8,4),(9,4),(10,4),(11,4),(12,4),(13,4)])
+
+
 ---------------------------------------
 
 ------------- Story Three ---------------
-
+-- Completes the move without any error handling (yet)
+-- If it doesn't work right let me (Sydney) know
+-- This function does not check if the game is over after a move or if a move is legal
 completeMove :: Game -> Move -> Game
-completeMove (turn, board) move = 
-    where
-        distributeMarbles :: Game -> Index -> Int -> (Board,Index) --Outputs the board after and the Index it landed on
-        distributeMarbles (turn, board) 
+completeMove (turn, board) move | landingIndex == 0 && turn == P2 = (P2, updatedBoard)
+                                | landingIndex == 7 && turn == P1 = (P1, updatedBoard)
+                                | amountAtIndex == 1 && isOnSide landingIndex turn = 
+                                    let opposite = landingIndex + 2*(7-landingIndex)
+                                        (Just oppositeMarbles) = lookup opposite updatedBoard
+                                        boardClearOpposite = changeValue opposite 0 updatedBoard
+                                        boardClearSame = changeValue landingIndex 0 boardClearOpposite
+                                        (Just storeValue) = if turn == P1 then lookup 7 boardClearSame else lookup 0 boardClearSame 
+                                        finalBoard = 
+                                            if turn == P1 
+                                            then changeValue 7 (storeValue+oppositeMarbles+1) boardClearSame
+                                            else changeValue 0 (storeValue+oppositeMarbles+1) boardClearSame
+                                    in (if turn == P1 then P2 else P1, finalBoard)
+                                | otherwise = ((if turn == P1 then P2 else P1), updatedBoard)
+    where   (Just numMarbles) = lookup move board
+            (distributeBoard, landingIndex, amountAtIndex) = distributeMarbles (turn, board) (nextIndex move turn) numMarbles 
+            updatedBoard = changeValue move 0 distributeBoard
+
+            distributeMarbles :: Game -> Index -> Int -> (Board,Index,Int) --Outputs the board after, the index landed on, and how many marbles in that pit
+            distributeMarbles (turn, board) index 1 = (addOneToIndex index board,index,numMarbles+1)
+                where (Just numMarbles) = lookup index board  
+            distributeMarbles (turn, board) index numMarbles = 
+                distributeMarbles (turn, addOneToIndex index board) (nextIndex index turn) (numMarbles-1)
+
+            nextIndex :: Index -> Turn -> Index 
+            nextIndex index turn | index == 13 && turn == P1 = 1
+                                | index == 6 && turn == P2 = 8
+                                | index == 13 = 0
+                                | otherwise = index + 1
+
+            addOneToIndex :: (Eq a, Num b) => a -> [(a,b)] -> [(a,b)]
+            addOneToIndex targetKey ((key,value):lst) = 
+                if key == targetKey
+                then (key,value+1):lst
+                else (key,value):(addOneToIndex targetKey lst) 
+
+            isOnSide :: Index -> Turn -> Bool
+            isOnSide index turn = (turn == P1 && index `elem` sideOne) || (turn == P2 && index `elem` sideTwo)
+
+            changeValue :: (Eq a, Num b) => a -> b -> [(a,b)] -> [(a,b)]
+            changeValue targetKey newValue ((key,value):lst) = 
+                if key == targetKey
+                then (key, newValue):lst
+                else (key, value):(changeValue targetKey newValue lst) 
         
 -----------------------------------------
 
@@ -41,7 +85,7 @@ completeMove (turn, board) move =
 -- -----------------------------------------
 
 prettyPrint :: Game -> String
-prettyPrint (turn,board) = "Current turn: "++(printPlayer currentTurn)++"\n"++
+prettyPrint (turn,board) = "Current turn: "++(printPlayer turn)++"\n"++
                             "-----------------------------------------"++"\n"++
                             "|   "++(prettyPrintSide board (reverse sideTwo))++"\n"++
                             (prettyPrintMiddle board)++"\n"++
