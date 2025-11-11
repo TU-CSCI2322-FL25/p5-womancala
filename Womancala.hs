e922397ab62dfeef516882af6874ebaca5f2c1ba
Dana Gingrey <dpg2208@hsutx.edu>
2025-11-11 11:28:17 -0600

    checkWinner without error handling

--- Womancala.hs
+++ Womancala.hs
@@ -4,8 +4,8 @@
 
 type Index = Int
 type Pit = (Index, Int) --(Index, Num of Marbles)
-pitOne = 0 --Scoring pit index for P1
-pitTwo = 7 --Scoring pit index for P2
+storeOne = 7 --Store index for P1
+storeTwo = 0 --Store index for P2
 sideOne = [1..6] --Indexes for P1 pits
 sideTwo = [8..13] -- Indexes for P2 pits
 type Board = [Pit] -- Will only ever be 14 long
@@ -16,5 +16,18 @@ currentTurn = P1
 type Winner = Maybe Player
 type Move = Index
 type Game = (Turn, Board)
+---------------------------------------
 
+------------- Story Two ---------------
+-- Check who has won the game state, if anyone, with a function of type  Game -> Winner.
+-- Without error handling for now (11-11)
+checkWinner :: Game -> Winner
+checkWinner (turn, board) =
+let 
+    pitP1 = lookup storeOne board
+    pitP2 = lookup storeTwo board
+in
+    if pitP1 > pitP2 then return Just P1
+    else if pit2 > pit1 then return Just P2
+    else Nothing 
 ---------------------------------------
